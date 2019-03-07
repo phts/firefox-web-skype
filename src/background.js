02f082494fb90bc4757e085c41070bf0294c149a
Phil Tsaryk <phil@tsaryk.com>
2019-03-07 21:08:08 +0100

    Add source files

---
+++ src/background.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const TARGET_PAGE = 'https://web.skype.com/*'
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.32 Safari/537.36'
+
+function rewriteUserAgentHeader(e) {
+  for (let header of e.requestHeaders) {
+    if (header.name.toLowerCase() === 'user-agent') {
+      header.value = USER_AGENT
+    }
+  }
+  return {requestHeaders: e.requestHeaders}
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  rewriteUserAgentHeader,
+  {urls: [TARGET_PAGE]},
+  ['blocking', 'requestHeaders'])
