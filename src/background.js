47dfffab4aaa4326cbd573d92fd9e5ab8a97e41c
Phil Tsaryk <phil@tsaryk.com>
2020-12-05 12:43:10 +0100

    feat: include more URLs as suggested in #8

--- src/background.js
+++ src/background.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const TARGET_URLS = [
-  'https://web.skype.com/*',
-  'https://preview.web.skype.com/*',
-  'https://join.skype.com/*',
-  'https://edge.skype.com/*',
+  'https://*.live.com/*',
+  'https://*.live.net/*',
+  'https://*.microsoft.com/*',
+  'https://*.microsoftonline.com/*',
+  'https://*.skype.com/*',
+  'https://*.skypeassets.com/*',
 ]
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 
